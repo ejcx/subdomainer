af3d1fe30ebaee43b62ab74fb09be345fe61c979
acjzz <jordiarnavat@gmail.com>
2014-06-24 12:57:49 +0200

    Added custom User-Agent feature

--- subdomainer.py
+++ subdomainer.py
@@ -4,6 +4,7 @@ import sys
 import re
 import time
 import httplib
+import random
 '''
 Created on Mar 2, 2013
 
@@ -18,6 +19,13 @@ but I think it needs more work. It will only find subdomains that have been inde
 search engines
 '''
 
+HEADERS = [
+    'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36',
+    'Mozilla/5.0 (Linux; U; Android4.2.2; zh-tw;WIZ T-218) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30;',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0'
+    # Add your customs UserAgents here
+]
+
 
 def curldom(domain):
     foundsubdomains = []
@@ -25,8 +33,11 @@ def curldom(domain):
 
     while True:
         try:
-                d = urllib2.urlopen("http://www.bing.com/search?q=site%3A" + domain + "&first=" + str(num)).read()
-                links =  d.split("<a href=")
+                request = urllib2.Request("http://www.bing.com/search?q=site%3A" + domain + "&first=" + str(num))
+                request.add_header('User-Agent', HEADERS[ random.randint( 1 , len( HEADERS ) ) - 1  ] )
+                opener = urllib2.build_opener()
+                feeddata = opener.open(request).read()
+                links =  feeddata.split("<a href=")
                 for i in links:
                     if "."+domain in i:
                         pagematches = i.split('"')
